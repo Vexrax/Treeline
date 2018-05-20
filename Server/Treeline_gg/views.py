90a2f9ad65c20a3d0c238810edc7ee1625cb6a83
Earleking <arek.fielding@gmail.com>
2018-05-20 13:40:59 -0400

    champ boxes stage 1

--- Server/Treeline_gg/views.py
+++ Server/Treeline_gg/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import HttpResponse, render
+from django.template import loader
+import json
+import os
+import collections
+
 
 
 def index(request):
     #return HttpResponse("lol")
-    return render(request, 'index.html')
+    #get current dir
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    #open champion json file
+    with open(os.path.join(BASE_DIR , '../www/static_data/data_files/champs.json')) as f:
+        data = json.load(f)
+
+    patch = "8.7.1"
+    #loop through json
+    for key in data:
+        data[key]["boxID"] = "champion_" + key
+        data[key]["url"] = "http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" + key + ".png"
+    #create context var
+    context = {
+        'champions': data,
+    }
+
+    #get template
+    template = loader.get_template("champPageBlock.html")
+    return HttpResponse(template.render(context, request))
