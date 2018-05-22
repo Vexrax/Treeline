62558ec1ff70b61139376bdfcbda762e8eaaeaec
Earleking <arek.fielding@gmail.com>
2018-05-22 16:49:39 -0400

    created utility scripts folder

---
+++ Server/Treeline_gg/utilityFiles/sortChampJson.py
@@ -0,0 +1,15 @@
+import json, operator
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+with open(os.path.join(BASE_DIR , '../../www/static_data/data_files/champs.json'), 'r') as f:
+    data = json.load(f)
+    
+with open(os.path.join(BASE_DIR , '../../www/static_data/data_files/champs.json'), 'w') as out:
+    json.dump(data, out, indent=4, sort_keys=True)
+
+
+
