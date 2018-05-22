033979bcc25933091f7473e89798ce243ac9c088
Earleking <arek.fielding@gmail.com>
2018-05-22 16:49:49 -0400

    added script to download champion icons

---
+++ Server/Treeline_gg/utilityFiles/downloadChampionIcons.py
@@ -0,0 +1,24 @@
+import requests
+
+import json
+
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+#get json of champions
+with open('../../../www/static_data/data_files/champs.json') as champFile:
+    data = json.load(champFile)
+    data = data["data"]
+
+patch = "8.7.1"
+#download and write
+for key in data:
+    print("Downloading image for: " + key)
+    img_data = requests.get("http://ddragon.leagueoflegends.com/cdn/" + patch + "/img/champion/" + key + ".png")
+    if not (img_data.ok):
+        print("Problem downloading image for: " + key)
+    else:
+        with open('../../../www/static_data/data_files/icons/' + key + '.png') as img:
+            img.write(img_data)
+            print("Done downloading image for: " + key)
+        
