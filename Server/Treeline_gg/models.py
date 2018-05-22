fde38d21ec504caf76fbcda58787d5870ff54933
Vexrax <joshuaf_111@live.com>
2018-05-22 17:53:29 -0400

    Updated models to conform with djangos "models" backend

--- Server/Treeline_gg/models.py
+++ Server/Treeline_gg/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class champion:
+class champion(models.Model):
+    name = models.CharField(max_length=30)
+
     def __init__(self, name):
         self.isChamp = self.checkIfChampExists()
-        self.name = name
         #more data like winrates etc
 
     def checkIfChampExists(self):
