b33f57060fd92c8d666ca0ec5f88e703a4da860a
Earleking <arek.fielding@gmail.com>
2018-05-26 12:36:17 -0400

    env works

--- Server/manage.py
+++ Server/manage.py
@@ -1,8 +1,17 @@
+
+
+
 #!/usr/bin/env python
 import os
 import sys
+from os.path import join, dirname
+import dotenv
 
 if __name__ == "__main__":
+    envpath = join(dirname(os.path.realpath(__file__)), '../.env')
+    dotenv.load_dotenv(dotenv_path=envpath)
+    #access env vars like this os.environ.get("RIOT_KEY"))
+    #print(os.getenv("RIOT_KEY")) 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Treeline.settings")
     try:
         from django.core.management import execute_from_command_line
