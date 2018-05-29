74059139f9b64f74982d00687ec5f995220a9259
Earleking <arek.fielding@gmail.com>
2018-05-29 10:11:45 -0400

    not needed

--- Server/Data_Files/Gathering/determineRole.py
+++
@@ -1,8 +0,0 @@
-"""this module will try and determine the role of the player"""
-import riotAPIReference
-
-# Soooo.... what do ya think about neural netting this thing lol
-
-def determineRoleWithGameID(game_id, game_data, participant_id):
-    game_timeline = riotAPIReference.getTimelineWithMatchID(game_id)
-    #lol now do stuff
